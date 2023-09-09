d91d26843f2c4b340666caf6895c762bc13f2d17
dias7367 <143860024+dias7367@users.noreply.github.com>
2023-09-10 00:24:44 +0700

    Add files via upload

---
+++ volume_tabung.py
@@ -0,0 +1,11 @@
+#menghitung volume tabung
+
+print("program menghitung volume tabung")
+
+pi = 3.14
+r = int(input("masukkan jari-jari tabung: "))
+t = int(input("masukkan tinggi tabung: "))
+
+V = pi*r**2*t
+print("rumus menghitung volume tabung V =pixr**2xt")
+print("hasil dari hitung volume tabung: ",V)
